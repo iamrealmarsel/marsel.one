edd58bfeef51f6e5d1d3c11c2a9e1ad55d7469e7
mars <maspam@yandex.ru>
2021-04-16 04:05:07 +0300

    Updates

--- src/components/PreloaderLine/PreloaderLine.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './PreloaderLine.scss';
-
-function PreloaderLine() {
-  return <div className={style.preloader}></div>;
-}
-
-export default PreloaderLine;
